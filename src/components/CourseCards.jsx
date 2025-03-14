84cf2aefebebc5390943c7274f1aa0eab771d3e3
Arisa-Kaewsuan <Arisa.Kaewsuan@gmail.com>
2025-03-14 19:00:13 +0700

    fix: icon

--- src/components/CourseCards.jsx
+++ src/components/CourseCards.jsx
@@ -5,7 +5,7 @@ const CourseCards = (props) => {
     <>
         <div className='bg-background md:bg-white'>
             <div className='px-8 bg-white md:bg-background md:w-90 py-5 mb-8 mx-5 md:mx-2 mt-2 rounded-2xl'>
-                <img src=".\src\assets\icon-run.svg" alt="icon"/>
+                <img src={props.icon} alt="icon"/>
                 <h4 className='py-9 text-2xl font-bold'>{props.title}</h4>
                 <p className='text-gray-500 text-lg'>{props.describe}</p>
                 <p className='text-lg text-pink-400 pt-6 font-bold'>Get Started</p>
