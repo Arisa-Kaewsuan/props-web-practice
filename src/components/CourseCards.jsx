5ff621b4d3910647d1005a240fe96a0e6c5c7f89
Arisa-Kaewsuan <Arisa.Kaewsuan@gmail.com>
2025-03-14 18:29:52 +0700

    feat: add Course Cards Section as a responsive

--- src/components/CourseCards.jsx
+++ src/components/CourseCards.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const CourseCards = (props) => {
   return (
     <>
-        <div className='bg-background'>
-            <div className='px-8 bg-white py-5 mb-8 mx-5 mt-2 rounded-2xl'>
+        <div className='bg-background md:bg-white'>
+            <div className='px-8 bg-white md:bg-background md:w-90 py-5 mb-8 mx-5 md:mx-2 mt-2 rounded-2xl'>
                 <img src=".\src\assets\icon-run.svg" alt="icon"/>
                 <h4 className='py-9 text-2xl font-bold'>{props.title}</h4>
                 <p className='text-gray-500 text-lg'>{props.describe}</p>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -28,9 +28,11 @@ const HomePage = () => {
 
             {/* Course Section */}
             <div className='flex flex-col items-center md:flex-row md:px-30'>
-                <h1 className='flex items-center mb-10 px-8 text-4xl font-bold text-start w-85 h-60 rounded-2xl bg-gradient-to-t from-pink-400 via-pink-200 to-salmon-400 text-white '>Check out our most popular courses!</h1>
-                <CourseCards title="Animation" describe="Learn the latest animation techniques to create stunning motion design and captivate your audience."/>
-                <CourseCards title="Design" describe="Create beautiful, usable interfaces to help shape the future of how the web looks."/>
+              <h1 className='flex items-center mb-10 px-8 text-4xl font-bold text-start w-85 h-60 md:h-84 rounded-2xl bg-gradient-to-t from-pink-400 via-pink-200 to-salmon-400 text-white '>Check out our most popular courses!</h1>
+              <CourseCards title="Animation" describe="Learn the latest animation techniques to create stunning motion design and captivate your audience."/>
+              <CourseCards title="Design" describe="Create beautiful, usable interfaces to help shape the future of how the web looks."/>
+            </div>
+            <div className='flex flex-row md:px-30'>
                 <CourseCards title="Photography" describe="Explore critical fundamentals like lighting, composition, and focus to capture exceptional photos."/>
                 <CourseCards title="Crypto" describe="All you need to know to get started investing in crypto. Go from beginner to advanced with this 54 hour course."/>
                 <CourseCards title="Business" describe="A step-by-step playbook to help you start, scale, and sustain your business without outside investment."/>
